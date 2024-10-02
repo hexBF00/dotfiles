7d08d442e13f1fcc7e99462e41d035eb03921e33
shell32 <hi@shell32.net>
2024-10-02 21:28:47 +0700

    refactor(setup.ps1): cleaning up

--- setup.ps1
+++ setup.ps1
@@ -1,19 +1,74 @@
-# Copyright (C) 2024 shell322 <hi@shell32.net>. All Rights Reserved.
+# Copyright (C) 2024 shell32 <hi@shell32.net>. All Rights Reserved.
 # See LICENSE file for more information on copyright and license.
+
+# vim: tabstop=4 shiftwidth=4 expandtab
 
 # Param
 param (
     [string]$profile = $null
 )
 
+# Variables
+$SCOOP_DIR = "$env:USERPROFILE\scoop"
+$SCOOP_BUCKET_DIR = "$SCOOP_DIR\buckets\main"
+$SCOOP_BUCKET_URL = "https://github.com/scoopinstaller/main/archive/master.zip"
+
 # Functions
+function Log-Log {
+    param (
+        [Parameter(Mandatory = $True, Position = 0)]
+        [String]$String,
+        [Parameter(Mandatory = $False, Position = 1)]
+        [System.ConsoleColor]$ForegroundColor = $host.UI.RawUI.ForegroundColor   
+    )
+
+    $old = $host.UI.RawUI.ForegroundColor
+    $host.UI.RawUI.ForegroundColor = $ForegroundColor
+    Write-Output "$String"
+    $host.UI.RawUI.ForegroundColor = $old
+}
+
+function Log-Info {
+    param (
+        [String]$message
+    )
+
+    Log-Log -String "[*] $message"
+}
+
+function Log-Task {
+    param (
+        [String]$message
+    )
+
+    Log-Log -String "[+] $message" -ForegroundColor Cyan
+}
+
+function Log-Warn {
+    param (
+        [String]$message
+    )
+
+    Log-Log -String "[!] $message" -ForegroundColor Yellow
+}
+
+function Log-Error {
+    param (
+        [String]$message,
+        [Int]$errorCode = 1
+    )
+
+    Log-Log -String "[!] $message" -ForegroundColor DarkRed
+    exit $errorCode
+}
+
 function Test-CommandAvaliable {
     param (
         [Parameter(Mandatory = $True, Position = 0)]
-	[String] $Command
+	    [String]$command
     )
 
-    return [Boolean](Get-Command $Command -ErrorAction SilentlyContinue)
+    return [Boolean](Get-Command $command -ErrorAction SilentlyContinue)
 }
 
 function Test-IsFileLocked {
@@ -38,7 +93,6 @@ function Test-IsFileLocked {
         }
         return $false
     } catch {
-        # The file is locked by a process.
         return $true
     }
 }
@@ -50,18 +104,16 @@ function Expand-ZipArchive {
     )
 
     if (!(Test-Path $path)) {
-	Write-Host "[!] Unzip failed: can't find $path to unzip" -ForegroundColor Red
-	exit 1
+        Log-Error "Unzip failed: can't unzip because a process is locking the file"
     }
 
     $retries = 0
     while ($retries -le 10) {
         if ($retries -eq 10) {
-	    Write-Host "[!] Unzip failed: can't unzip because a process is locknig the file" -ForegroundColor Red
-	    exit 1
+            Log-Error "Unzip failed: can't unzip because a process is locking the file"
         }
         if (Test-IsFileLocked $path) {
-	    Write-Host "[*] Unzip: waiting for $path to be unlocked by another process... ($retries/10)"
+            Log-Info "Unzip: waiting for $path to be unlocked by another process... ($retries/10)"
             $retries++
             Start-Sleep -Seconds 2
         } else {
@@ -80,153 +132,169 @@ function Expand-ZipArchive {
     $global:ProgressPreference = $oldProgressPreference
 }
 
-# Variables
-$SCOOP_DIR = "$env:USERPROFILE\scoop"
-$SCOOP_MAIN_BUCKET_DIR = "$SCOOP_DIR\buckets\main"
-$SCOOP_MAIN_BUCKET_URL = "https://github.com/shlwapidll/main/archive/main.zip"
+function Check-PwshVersion {
+    Log-Task "Checking if PowerShell version is compatible..."
+    if ($PSVersionTable.PSVersion -lt [System.Version]"5.0.0") {
+        Log-Error "It seems like you are using old powershell version. Please update to newer version"
+    }
+}
+
+function Check-RunAsAdmin {
+    Log-Task "Checking if script is running as administrator..."
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if (!($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))) {
+        Log-Error "Please run this script as administrator"
+    }
+}
+
+function Check-ExecPolicy {
+    Log-Task "Checking if execution policy is unrestricted..."
+    if ((Get-ExecutionPolicy).ToString() -ne "Unrestricted") {
+        Log-Error "Please update execution policy to unrestricted"
+    }
+}
+
+function Check-OS {
+    Log-Task "Checking if Operation System is compatible..."
+    if ((Get-WmiObject -class Win32_OperatingSystem).Version -eq "6.1.7601") {
+        Log-Error "Windows 7 or below is not supported"
+    }
+}
+
+function Check-VM {
+    Log-Task "Checking if machine is vurtual machine"
+    $virtualModels = @("VirtualBox", "VMware", "Virtual Machine", "Hyper-V")
+    $computerSystemModel = (Get-WmiObject win32_computersystem).model
+    $isVirtualModel = $false
+
+    foreach ($model in $virtualModels) {
+        if ($computerSystemModel.Contains($model)) {
+            $isVirtualModel = $true
+            break
+        }
+    }
+
+    if (!isVirtualModel) {
+        Log-Warn "You are not on virtual machine or have hardened your machine to not appear as a virtual machine"
+        Log-Warn "DO NOT INSTALL this on your host system"
+        Write-Host "[-] Do you still wish to proceed? (y/N): " -NoNewline
+        $response = Read-Host
+        if ($response -notin @("y", "Y")) {
+            exit 1
+        } 
+    }
+}
+
+function Check-TamperProtection {
+    Log-Task "Checking if Windows Defender Tamper Protection is disabled..."
+    try {
+        $tp = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender\Features" -Name "TamperProtection" -ErrorAction Stop
+        if ($tp.TamperProtection -eq 5) {
+            Log-Error "Please disable Tamper Protection before running this script"
+        }
+    } catch {
+        Log-Warn "Failed to check if Tamper Protection is enabled or not"
+        Write-Host "[-] Do you still wish to proceed? (y/N): " -NoNewline
+        $response = Read-Host
+        if ($response -notin @("y", "Y")) {
+            exit 1
+        } 
+    }
+}
+
+function Check-WinDefend {
+    Log-Task "Checking if Windows Defender service is disabled..."
+    $defender = Get-Service -Name WinDefend -ea 0
+    if ($null -ne $defender) {
+        if ($defender.Status -eq "Running") {
+            Log-Error "Please disable Windows Defender through Group Policy before running this script"
+        }
+    }
+}
+
+function Task-PassNeverExpire {
+    Log-Task "Set password to never expire..."
+    Set-LocalUser -Name "${Env:UserName}" -PasswordNeverExpires $true
+}
+
+function Task-SetPowerOpts {
+    Log-Task "Set power options to prevent install from timing out..."
+    powercfg -change -monitor-timeout-ac 0 | Out-Null
+    powercfg -change -monitor-timeout-dc 0 | Out-Null
+    powercfg -change -disk-timeout-ac 0 | Out-Null
+    powercfg -change -disk-timeout-dc 0 | Out-Null
+    powercfg -change -standby-timeout-ac 0 | Out-Null
+    powercfg -change -standby-timeout-dc 0 | Out-Null
+    powercfg -change -hibernate-timeout-ac 0 | Out-Null
+    powercfg -change -hibernate-timeout-dc 0 | Out-Null
+}
+
+function Task-ConfigScoop {
+    if (!(Test-CommandAvaliable("scoop"))) {
+        Log-Task "Installing scoop..."
+        & ([scriptblock]::Create((irm "https://get.scoop.sh"))) -RunAsAdmin | Out-Null
+    }
+
+    Log-Task "Configurating scoop..."
+    scoop bucket rm main | Out-Null
+
+    if (!(Test-Path $SCOOP_BUCKET_DIR)) {
+        New-Item -Type Directory $SCOOP_BUCKET_DIR | Out-Null
+    }
+
+    $scoopBucketArchive = "$SCOOP_BUCKET_DIR\_bucket.zip"
+    $scoopBucketExtract = "$SCOOP_BUCKET_DIR\_tmp"
+
+    $downloadSession = New-Object System.Net.WebClient
+    $downloadSession.downloadFile($SCOOP_BUCKET_URL, $scoopBucketArchive)
+
+    Expand-ZipArchive $scoopBucketArchive $scoopBucketExtract
+
+    Copy-Item "$scoopBucketExtract\Main-*\*" $SCOOP_BUCKET_DIR -Recurse -Force
+
+    Remove-Item $scoopBucketArchive
+    Remove-Item $scoopBucketExtract -Recurse -Force
+}
 
 # Main
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
 if ($profile -notin @("workstation", "vm")) {
+    Write-Output "Usage: .\setup.ps1 [workstation/vm]"
     exit 1
 }
 
+## Checking requirement
 if (!([Environment]::Is64BitProcess)) {
-    Write-Host "[!] It seems like you are using 32bit powershell. Please use 64 bit instead" -ForegroundColor Red
-    exit 1
+    Log-Error "It seems like you are using 32 bit powershell. Please use 64 bit instead"
 }
 
-Write-Host "[+] Checking if PowerShell version is compatible..."
-if ($PSVersionTable.PSVersion -lt [System.Version]"5.0.0") {
-    Write-Host "[!] It seems like you are using old powershell version. Please update to newer version" -ForegroundColor Red
-    exit 1
-}
-
-Write-Host "[+] Checking if script is running as administrator..."
-$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-if (-Not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "[!] Please run this script as administrator." -ForegroundColor Red
-    exit 1
-}
-
-Write-Host "[+] Checking if execution policy is unrestricted..."
-if ((Get-ExecutionPolicy).ToString() -ne "Unrestricted") {
-    Write-Host "[!] Please update execution policy to unrestricted" -ForegroundColor Red
-    exit 1
-}
-
-Write-Host "[+] Checking if Operation System is compatible..."
-if ((Get-WmiObject -class Win32_OperatingSystem).Version -eq "6.1.7601") {
-    Write-Host "[!] Windows 7 or below is not supported" -ForegroundRed
-    exit 1
-}
-
+Check-PwshVersion
+Check-RunAsAdmin
+Check-ExecPolicy
+Check-OS
 if ($profile -eq "vm") {
-    Write-Host "[+] Checking if machine is a virtual machine"
-    $virtualModels = @("VirtualBox", "VMware", "Virtual Machine", "Hyper-V")
-    $computerSystemModel = (Get-WmiObject win32_computersystem).model
-    $isVirtualModel = $false
-
-    foreach ($model in $virtualModels) {
-	if ($computerSystemModel.Contains($model)) {
-	    $isVirtualModel = $true
-	    break
-        }
-    }
-
-    if (!$isVirtualModel) {
-	Write-Host "[!] You are not on virtual machine or have hardened your machine to not appear as a virtual machine" -ForegroundColor Red
-	Write-Host "[!] DO NOT INSTALL this on your host system" -ForegroundColor Red
-	Write-Host "[-] Do you still wish to proceed? (y/N): " -NoNewline
-	$response = Read-Host
-	if ($response -notin @("y", "Y")) {
-	    exit 1
-        }
-    }
+    Check-VM
 }
+Check-TamperProtection
+Check-WinDefend
 
-Write-Host "[+] Checking if Windows Defender Tamper Protection is disabled..."
-try {
-    $tpEnabled = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows Defender\Features" -Name "TamperProtection" -ErrorAction Stop
-    if ($tpEnabled.TamperProtection -eq 5) {
-	Write-Host "[!] Please disable Tamper Protection before running this script." -ForegroundColor Red
-        exit 1
-    }
-} catch {
-    Write-Host "[!] Failed to check if Tamper Protection is enabled or not" -ForegroundColor Yellow
-    Write-Host "[-] Do you still wish to proceed? (y/N): " -NoNewline
-    $response = Read-Host
-    exit 1
-}
-
-Write-Host "[+] Checking if Windows Defender service is disabled..."
-$defender = Get-Service -Name WinDefend -ea 0
-if ($null -ne $defender) {
-    if ($defender.Status -eq "Running") {
-        Write-Host "[!] Please disable Windows Defender through Group Policy before running this script" -ForegroundColor Red
-	exit 1
-    }
-}
-
+## Print out info
 $desktopPath = [Environment]::GetFolderPath("Desktop")
 Set-Location -Path $desktopPath -PassThru | Out-Null
 
 Clear-Host
-Write-Host "[*] Welcome to shell32's environment setup script"
-Write-Host "[*] Profile: $profile"
+Log-Info "Welcome to shell32's environment bootstrap script"
+Log-Info "Profile: $profile"
 Write-Host "[-] Do you wish to proceed? (y/N): " -NoNewline
 $response = Read-Host
 if ($response -notin @("y", "Y")) {
     exit 1
 }
+Write-Host
 
-Write-Host ""
-
-$downloadSession = New-Object System.Net.WebClient
-
-Write-Host "[+] Set password to never expire..." -ForegroundColor Cyan
-Set-LocalUser -Name "${Env:UserName}" -PasswordNeverExpires $true
-
-Write-Host "[+] Set power options to prevent install from timing out..." -ForegroundColor Cyan
-powercfg -change -monitor-timeout-ac 0 | Out-Null
-powercfg -change -monitor-timeout-dc 0 | Out-Null
-powercfg -change -disk-timeout-ac 0 | Out-Null
-powercfg -change -disk-timeout-dc 0 | Out-Null
-powercfg -change -standby-timeout-ac 0 | Out-Null
-powercfg -change -standby-timeout-dc 0 | Out-Null
-powercfg -change -hibernate-timeout-ac 0 | Out-Null
-powercfg -change -hibernate-timeout-dc 0 | Out-Null
-
-if (!(Test-CommandAvaliable("scoop"))) {
-    Write-Host "[+] Installing scoop..." -ForegroundColor Cyan
-    & ([scriptblock]::Create((irm "https://get.scoop.sh"))) -RunAsAdmin
-}
-
-Write-Host "[+] Configuration scoop..." -ForegroundColor Cyan
-scoop bucket rm main
-
-$scoopMainZipfile = "$SCOOP_MAIN_BUCKET_DIR\scoop-main.zip"
-$scoopMainUnzipTempDir = "$SCOOP_MAIN_BUCKET_DIR\_tmp"
-
-if (!(Test-Path $SCOOP_MAIN_BUCKET_DIR)) {
-    New-Item -Type Directory $SCOOP_MAIN_BUCKET_DIR | Out-Null
-}
-
-$downloadSession.downloadFile($SCOOP_MAIN_BUCKET_URL, $scoopMainZipfile)
-
-Expand-ZipArchive $scoopMainZipfile $scoopMainUnzipTempDir
-Copy-Item "$scoopMainUnzipTempDir\Main-*\*" $SCOOP_MAIN_BUCKET_DIR -Recurse -Force
-
-Remove-Item $scoopMainZipfile
-Remove-Item $scoopMainUnzipTempDir -Recurse -Force
-
-Write-Host "[+] Debloating windows..." -ForegroundColor Cyan
-$debloatScript = {& ([scriptblock]::Create((irm "https://win11debloat.raphi.re/"))) -RunDefaults -Silent}
-$debloatScriptEncoded = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($debloatScript))
-$debloatProcess = Start-Process powershell.exe -PassThru -Verb RunAs -ArgumentList "-encodedCommand", $debloatScriptEncoded
-
-if ($null -ne $debloatProcess) {
-    $debloatProcess.WaitForExit()
-}
+## Run bootstrap
+Task-PassNeverExpire
+Task-SetPowerOpts
+Task-ConfigScoop
